0a6d46b4c2ca8dc1e476e9d4c34f09b30e9ffd5c
Anastasia Mizumskaya <beyoncem7@gmail.com>
2017-06-01 00:33:03 +0300

    repo set up

---
+++ Swipe.Xamarin.Forms.Controls/Swipe.Xamarin.Forms.Controls/Swipe.Xamarin.Forms.ControlsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Swipe.Xamarin.Forms.Controls
+{
+	public partial class Swipe_Xamarin_Forms_ControlsPage : ContentPage
+	{
+		public Swipe_Xamarin_Forms_ControlsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
